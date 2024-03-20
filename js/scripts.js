62b16afd9a2a5e3131f43a264ac82841d621737b
eryncraig <erynlcraig@gmail.com>
2024-03-20 14:04:05 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+// let bigMultiplication = 1345 * 2309;
+// document.write(bigMultiplication)
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize)
