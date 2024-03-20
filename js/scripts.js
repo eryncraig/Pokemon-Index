6f4c0917645fee9e23e4d8d1dd4d4265c198311a
eryncraig <erynlcraig@gmail.com>
2024-03-20 14:16:10 -0700

    added new JS variables for testing

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,4 @@
-let myName = 'Bob';
-document.write(myName);
-myName = 'John Doe';
-document.write(myName);
+alert('Hello World');
 
-// let bigMultiplication = 1345 * 2309;
-// document.write(bigMultiplication)
-
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize)
+let favoriteFood = 'Panang Curry';
+document.write(favoriteFood);
