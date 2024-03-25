af7239bc1521a904ac61296ea50cf2fc6d01ac92
eryncraig <erynlcraig@gmail.com>
2024-03-25 14:31:37 -0700

    Fixed for loop with added conditionals to print more info to the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,20 @@
+//placeholder array until API is connected, using to test functionality
 let pokemonList = [
   { name: 'Beedrill', weight: 125, types: ['bug ', 'poison '] },
   { name: 'Bulbasaur', weight: 240, types: ['grass ', 'poison '] },
   { name: 'Zebstrika', weight: 350, types: ['normal ', 'electric '] }
 ];
-let text = '';
-let i = 0;
 
-for (; pokemonList[i];) {
-  text = text + ' ' + pokemonList[i].name + ' ' + '(' + 'weight: ' + pokemonList[i].weight + ')';
-  i++;
-} console.log(text);
-document.write(text);
-
+//different style of 'for' loop with conditionals inside to print different things based on pokemon weight. Originally had a shorthand 'for' loop version
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].weight > 100 && pokemonList[i].weight < 200) {
+    console.log('medium');
+    document.write(pokemonList[i].name + ' ' + '(' + 'weight: ' + pokemonList[i].weight + ')' + ' It\'s a medium pokemon. ')
+  } else if (pokemonList[i].weight < 100) {
+    console.log('small');
+    document.write(pokemonList[i].name + ' ' + '(' + 'weight: ' + pokemonList[i].weight + ')' + ' That pokemon is so small! ')
+  } else {
+    console.log('big');
+    document.write(pokemonList[i].name + ' ' + '(' + 'weight: ' + pokemonList[i].weight + ')' + ' Wow! That\'s a big pokemon! ')
+  }
+}
