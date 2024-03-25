e60e0bea685edc3c30e8b27b2416dd4ad172e6de
eryncraig <erynlcraig@gmail.com>
2024-03-25 14:15:38 -0700

    added for loop to current placeholder array in order to display pokemon names and weights

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 let pokemonList = [
-  { name: 'Beedrill', weight: 125, types: ['bug', 'poison'] },
-  { name: 'Bulbasaur', weight: 240, types: ['grass', 'poison'] },
-  { name: 'Zebstrika', weight: 350, types: ['normal', 'electric'] }
+  { name: 'Beedrill', weight: 125, types: ['bug ', 'poison '] },
+  { name: 'Bulbasaur', weight: 240, types: ['grass ', 'poison '] },
+  { name: 'Zebstrika', weight: 350, types: ['normal ', 'electric '] }
 ];
+let text = '';
+let i = 0;
+
+for (; pokemonList[i];) {
+  text = text + ' ' + pokemonList[i].name + ' ' + '(' + 'weight: ' + pokemonList[i].weight + ')';
+  i++;
+} console.log(text);
+document.write(text);
 
