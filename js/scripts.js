8cf0db3805c5297857e9268bb05f85123a4ff6f1
eryncraig <erynlcraig@gmail.com>
2024-03-25 12:17:44 -0700

    Added 3 pokemon to scripts
    Adding pokemon objects (and arrays) in array to begin building the app interface before loading all data

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
-let pokemonList = []
+let pokemonList = [
+  { name: 'Beedrill', weight: 125, types: ['bug', 'poison'] },
+  { name: 'Bulbasaur', weight: 240, types: ['grass', 'poison'] },
+  { name: 'Zebstrika', weight: 350, types: ['normal', 'electric'] }
+];
+
