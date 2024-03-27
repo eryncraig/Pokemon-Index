1332245df946f7e5dd698d741846610fc5278707
eryncraig <erynlcraig@gmail.com>
2024-03-27 09:06:23 -0700

    Added styling to output, basic font, colors, and display

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,16 @@ let pokemonList = [
   { name: 'Zebstrika', weight: 350, types: ['normal ', 'electric '] }
 ];
 
-//different style of 'for' loop with conditionals inside to print different things based on pokemon weight. Originally had a shorthand 'for' loop version
+//different style of 'for' loop with conditionals inside to print different things based on pokemon weight. Originally had a shorthand 'for' loop version but preferred conditions be set this way for scaling
 for (let i = 0; i < pokemonList.length; i++) {
   if (pokemonList[i].weight > 100 && pokemonList[i].weight < 200) {
     console.log('medium');
-    document.write(pokemonList[i].name + ' ' + '(' + 'weight: ' + pokemonList[i].weight + ')' + ' It\'s a medium pokemon. ')
+    document.write('<div>' + pokemonList[i].name + ' ' + '(' + 'weight: ' + pokemonList[i].weight + ')' + ' It\'s a medium pokemon.</div>')
   } else if (pokemonList[i].weight < 100) {
     console.log('small');
-    document.write(pokemonList[i].name + ' ' + '(' + 'weight: ' + pokemonList[i].weight + ')' + ' That pokemon is so small! ')
+    document.write('<div>' + pokemonList[i].name + ' ' + '(' + 'weight: ' + pokemonList[i].weight + ')' + ' That pokemon is so small! </div> ')
   } else {
     console.log('big');
-    document.write(pokemonList[i].name + ' ' + '(' + 'weight: ' + pokemonList[i].weight + ')' + ' Wow! That\'s a big pokemon! ')
+    document.write('<div>' + pokemonList[i].name + ' ' + '(' + 'weight: ' + pokemonList[i].weight + ')' + ' Wow! That\'s a big pokemon!</div>')
   }
 }
